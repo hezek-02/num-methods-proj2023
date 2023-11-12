4f73283843b258fb3658075a3ae3516767d7fe40
Nery Ezequiel Capo Brazeiro <nery.ezequiel.capo@fing.edu.uy>
2023-11-12 01:50:29 -0300

    Update jacobiana.m, elimino licencia, se crea parametro minCuad que identifica si es un problema de minimosCuadrados con minCuad nros de ecuaciones

--- Obligatorio/jacobiana.m
+++ Obligatorio/jacobiana.m
@@ -1,26 +1,3 @@
-## Copyright (C) 2023 Ezequiel Capó
-##
-## This program is free software: you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
-## (at your option) any later version.
-##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-## -*- texinfo -*-
-## @deftypefn {} {@var{retval} =} Jacobiana(@var{input1}, @var{input2})
-##
-## @seealso{}
-## @end deftypefn
-
-## Author: Ezequiel Capó <hezek@heze-Makina>
-## Created: 2023-11-09
 
 function retval = jacobiana(x,S,minCuad)
   c = 299792.458; %km/s
