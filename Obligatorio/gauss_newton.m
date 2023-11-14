18dd820cfa597f99e8bcf9d97183679a531cebeb
Lucas Fernando Goncalves Ramos <lucas.goncalves@fing.edu.uy>
2023-11-14 12:40:11 -0300

    Update gauss_newton.m

--- Obligatorio/gauss_newton.m
+++ Obligatorio/gauss_newton.m
@@ -1,16 +1,16 @@
 
-
 function [x,k] = gauss_newton(x0, y, JF, F, tol, itMax)
-  x = x0;
+  x = x0';
   k = 0;
   dif = inf;
   while (k <itMax) && (norm(dif) > tol)
-      r = F(x) - y;
-      [U,S,V] = svd(JF(x));
+      r = F(x') - y;
+      [U,S,V] = svd(JF(x'))
       z = U' * (-r);
-      w = S(1:4,1:4) \ z(1:4);
-      dif = V*w;
-      x = x + dif;
+      n = size(x,1);
+      w = S(1:n,1:n) \ z(1:n);
+      dif = V*w
+      x = x + dif
       k = k+1;
  endwhile
 endfunction
